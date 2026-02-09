ee3556044d07a68d645e1d4d994335927d3788cd
Jerry Wan <Haoxuan.wan@education.nsw.gov.au>
2026-02-10 10:40:14 +1100

    Changing the project a bit

--- GeoRanking/Countries/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Country(models.Model):
-  name = models.CharField(max_length=255, default="Unknown")
-  capital = models.CharField(max_length=255, default="Unknown")
-  currency = models.CharField(max_length=100, default="USD")
-  safety_index = models.FloatField(default="0")
-  
--- GeoRanking/Countries/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.all_countries, name='all_countries'),
-    path('<int:id>/', views.country_detail, name='country_detail'),
-]
