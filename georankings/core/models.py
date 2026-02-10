8c23d32ee18562c23f262ee73f961f26a7314ae1
Jerry Wan <Haoxuan.wan@education.nsw.gov.au>
2026-02-11 10:26:32 +1100

    HmtlCss and some java script

--- georankings/core/models.py
+++ georankings/core/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Country(models.Model):
+  name = models.CharField(max_length=100)
+
+
+def __str__(self):
+  return self.name
+
+
+class Rating(models.Model):
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  country = models.ForeignKey(Country, on_delete=models.CASCADE)
+  score = models.IntegerField()
+
+
+class Meta:
+  unique_together = ('user', 'country')
+
+
+class BlogPost(models.Model):
+  author = models.ForeignKey(User, on_delete=models.CASCADE)
+  title = models.CharField(max_length=200)
+  content = models.TextField()
+  created = models.DateTimeField(auto_now_add=True)
+
+
+def __str__(self):
+  return self.title
--- georankings/core/views.py
+++ georankings/core/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from urllib3 import request
+from .models import Country, Rating, BlogPost
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+
+
+
+def home(request):
+  posts = BlogPost.objects.order_by('-created')
+  return render(request, 'home.html', {'posts': posts})
+
+
+@login_required
+def rate_country(request):
+  countries = Country.objects.all()
+  if request.method == 'POST':
+    country_id = request.POST['country']
+    score = request.POST['score']
+    Rating.objects.update_or_create(
+      user=request.user,
+      country_id=country_id,
+      defaults={'score': score}
+
+)
+    return redirect('home')
+  return render(request, 'rate.html', {'countries': countries})
+
+
+def register(request):
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+  if form.is_valid():
+    form.save()
+    return redirect('login')
+  else:
+    form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
