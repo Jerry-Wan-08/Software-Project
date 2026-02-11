4ff6bf184205f9800da65fd5f4c0a044fdd1aa3f
Jerry Wan <Haoxuan.wan@education.nsw.gov.au>
2026-02-11 12:59:19 +1100

    Fixed countries and added a nonfunctioning signup page login in page does work tho

--- georankings/core/models.py
+++ georankings/core/models.py
@@ -1,51 +1,57 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
 class Country(models.Model):
-  name = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100, unique=True)
 
-  safety_index = models.DecimalField(
-      max_digits=5,
-      decimal_places=2,
-      help_text="Safety index out of 100"
-  )
+    safety_index = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+        help_text="Safety index out of 100"
+    )
 
-  cost_index = models.DecimalField(
-      max_digits=5,
-      decimal_places=2,
-      help_text="Cost of living index (higher = more expensive)"
-  )
+    cost_index = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+        help_text="Cost of living index (higher = more expensive)"
+    )
 
-  travel_rating = models.DecimalField(
-      max_digits=3,
-      decimal_places=1,
-      help_text="Overall travel rating out of 10"
-  )
+    travel_rating = models.DecimalField(
+        max_digits=3,
+        decimal_places=1,
+        help_text="Overall travel rating out of 10"
+    )
 
-  def __str__(self):
-      return self.name
-    
-def __str__(self):
-  return self.name
+    def __str__(self):
+        return self.name
+
+    def average_user_rating(self):
+        """
+        Returns the average rating given by users for this country.
+        """
+        ratings = self.rating_set.all()
+        if ratings.exists():
+            return round(sum(r.score for r in ratings) / ratings.count(), 2)
+        return None
 
 
 class Rating(models.Model):
-  user = models.ForeignKey(User, on_delete=models.CASCADE)
-  country = models.ForeignKey(Country, on_delete=models.CASCADE)
-  score = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    score = models.IntegerField()
 
+    class Meta:
+        unique_together = ('user', 'country')
 
-class Meta:
-  unique_together = ('user', 'country')
+    def __str__(self):
+        return f"{self.user.username} - {self.country.name}: {self.score}"
 
 
 class BlogPost(models.Model):
-  author = models.ForeignKey(User, on_delete=models.CASCADE)
-  title = models.CharField(max_length=200)
-  content = models.TextField()
-  created = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
 
-
-def __str__(self):
-  return self.title
+    def __str__(self):
+        return self.title
--- georankings/core/views.py
+++ georankings/core/views.py
@@ -1,39 +1,35 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from urllib3 import request
-from .models import Country, Rating, BlogPost
 from django.contrib.auth.forms import UserCreationForm
-
-
-
+from django.contrib.auth import login
+from .models import Country, Rating, BlogPost
 
 def home(request):
-  posts = BlogPost.objects.order_by('-created')
-  return render(request, 'home.html', {'posts': posts})
-
+    posts = BlogPost.objects.order_by('-created')
+    countries = Country.objects.all()
+    return render(request, 'home.html', {'posts': posts, 'countries': countries})
 
 @login_required
 def rate_country(request):
-  countries = Country.objects.all()
-  if request.method == 'POST':
-    country_id = request.POST['country']
-    score = request.POST['score']
-    Rating.objects.update_or_create(
-      user=request.user,
-      country_id=country_id,
-      defaults={'score': score}
-
-)
-    return redirect('home')
-  return render(request, 'rate.html', {'countries': countries})
-
+    countries = Country.objects.all()
+    if request.method == 'POST':
+        country_id = request.POST['country']
+        score = int(request.POST['score'])
+        Rating.objects.update_or_create(
+            user=request.user,
+            country_id=country_id,
+            defaults={'score': score}
+        )
+        return redirect('home')
+    return render(request, 'rate.html', {'countries': countries})
 
 def register(request):
-  if request.method == 'POST':
-    form = UserCreationForm(request.POST)
-  if form.is_valid():
-    form.save()
-    return redirect('login')
-  else:
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
     return render(request, 'register.html', {'form': form})
